a5fe82e081cfc248567eb44737b780338afd8be8
Graham Marlow <mgmarlow@hey.com>
2022-12-07 19:19:44 -0800

    fixup! Day 7 WIP

--- src/day07.lisp
+++ src/day07.lisp
@@ -1,67 +1,66 @@
 (load "~/.quicklisp/setup.lisp")
 (ql:quickload "cl-utilities")
 
-(defclass command ()
+(defclass directory-node ()
   ((name
-    :initarg :command
-    :accessor command)
+    :initarg :name
+    :accessor name)
+   (children
+    :initarg :children
+    :initform '()
+    :accessor children)
+   (parent
+    :initarg :parent
+    :accessor parent)
+   (size
+    :initarg :size
+    :initform 0
+    :accessor size)))
+
+(defclass file-leaf ()
+  ((name
+    :initarg :name
+    :accessor name)
+   (size
+    :initarg :size
+    :accessor size)))
+
+(defclass command ()
+  ((kind
+    :initarg :kind
+    :accessor kind)
    (value
     :initarg :value
     :accessor value)))
 
-(defclass resource ()
-  ((kind
-    :initarg :kind
-    :accessor kind)
-   (name
-    :initarg name
-    :accessor name)
-   (size
-    :initarg :size
-    :initform 0
-    :accessor size)
-   (children
-    :initarg :children
-    :accessor children)))
+(defun cd-commandp (parsed-line)
+  (string= (second parsed-line) "cd"))
 
 (defun commandp (line)
   (string= (char line 0) #\$))
 
-(defun new-command (line)
-  (let ((parsed (cl-utilities:split-sequence #\SPACE line)))
-    (if (string= (second parsed) "cd")
-        (make-instance 'command
-                       :name "cd"
-                       :value (third parsed))
-        (make-instance 'command
-                       :name "ls"
-                       :value '()))))
-
-(defun new-resource (line)
-  (let ((parsed (cl-utilities:split-sequence #\SPACE line)))
-    (if (string= (first parsed) "dir")
-        (make-instance 'resource
-                       :kind directory
-                       :name (second parsed))
-        (make-instance 'resource
-                       :kind file
-                       :name (second parsed)
-                       :size (first parsed)))))
-
-;; (defparameter *raw-input*
-;;   (with-open-file (in "../data/day06.txt")
-;;     (loop for line = (read-line in nil)
-;;           while line
-;;           collect line)))
-
-(defun build-file-system ()
-  (let ((stack '()))
-    (with-open-file (in "../data/day06.txt")
+(defun parse-commands ()
+  (let ((commands '()) (latest-ls))
+    (with-open-file (in "../data/day07.txt")
       (loop for line = (read-line in nil)
             while line
             when (> (length line) 0) do
-              (cond
-                ((commandp line) 'todo-create-command-and-update-state)
-                (t 'todo-build-children))))
-    'todo))
+              (cond ((commandp line)
+                     (let ((parsed (cl-utilities:split-sequence #\SPACE line)))
+                       (if (cd-commandp parsed)
+                           (push (make-instance 'command
+                                                :kind "cd"
+                                                :value (third parsed))
+                                 commands)
+                           (progn
+                             (let ((ls-command (make-instance 'command :kind "ls" :value '()))) 
+                               (push ls-command commands)
+                               (setf latest-ls ls-command))))))
+                    (t (push line (value latest-ls)))))
+      (nreverse commands))))
+
+(defun build-file-system ()
+  (let* ((root (make-instance 'directory-node :name "/")) (cwd root))
+    ;; todo
+    root))
 
