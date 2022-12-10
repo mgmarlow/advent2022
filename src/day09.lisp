ddfaf08bcc549f7a1b0cdc759a63144a8cabf4b7
Graham Marlow <mgmarlow@hey.com>
2022-12-10 09:24:22 -0800

    Day 9 solution

--- src/day09.lisp
+++ src/day09.lisp
@@ -1,10 +1,16 @@
+(load "~/.quicklisp/setup.lisp")
+(ql:quickload "cl-utilities")
+
 (defun flatten (l)
   "Utility function for flattening a list of lists."
   (cond ((null l) nil)
         ((atom l) (list l))
         (t (loop for a in l appending (flatten a)))))
 
-(defun sign (x) (if (> x 0) 1 -1))
+(defun sign (x)
+  (cond ((> x 0) 1)
+        ((< x 0) -1)
+        (t 0)))
 
 (defclass point ()
   ((x
@@ -19,6 +25,9 @@
 (defun new-point (x y)
   (make-instance 'point :x x :y y))
 
+(defmethod point-zerop ((p point))
+  (and (= (x p) 0) (= (y p) 0)))
+
 (defmethod subtract ((a point) (b point))
   (make-instance 'point
                  :x (- (x a) (x b))
@@ -28,15 +37,13 @@
   (setf (x p) (+ (x p) (x dir)))
   (setf (y p) (+ (y p) (y dir))))
 
-;; (defmethod diagonalp ((p point) (other point))
-;;   (let ((delta (subtract p other)))
-;;     (and (>= (abs (x delta)) 1) (>= (abs (y delta)) 1))))
-
 (defun create-points (dir n)
   (loop for i from 0 to (1- n) collect (new-point (x dir) (y dir))))
 
 (defun expand-command (line)
-  (let ((dir (char line 0)) (n (digit-char-p (char line 2))))
+  (let* ((parsed (cl-utilities:split-sequence #\SPACE line))
+         (dir (first parsed))
+         (n (parse-integer (second parsed))))
     (cond
       ((string= dir "R") (create-points (new-point 1 0) n))
       ((string= dir "L") (create-points (new-point -1 0) n))
@@ -52,24 +59,26 @@
            when (> (length line) 0)
              collect (expand-command line)))))
 
+(defun diagonalp (delta)
+  (and (> (abs (x delta)) 0) (> (abs (y delta)) 0)))
+
 (defun find-next-tail-move (rhead rtail)
   (let ((delta (subtract rhead rtail)))
     (cond
-      ((and (and (>= (abs (x delta)) 1) (>= (abs (y delta)) 1))
-            (or (= (abs (x delta)) 2) (= (abs (y delta)) 2)))
+      ((and (diagonalp delta)
+            (or (> (abs (x delta)) 1) (> (abs (y delta)) 1)))
        (new-point (sign (x delta)) (sign (y delta))))
-      ((>= (abs (x delta)) 2) (new-point (sign (x delta)) 0))
-      ((>= (abs (y delta)) 2) (new-point 0 (sign (y delta))))
+      ((> (abs (x delta)) 1) (new-point (sign (x delta)) 0))
+      ((> (abs (y delta)) 1) (new-point 0 (sign (y delta))))
       (t (new-point 0 0)))))
 
 (defun p1-find-visited-tail-positions ()
-  (let ((positions '()) (head (new-point 0 0)) (tail (new-point 0 0)))
+  (let ((positions '("0,0")) (head (new-point 0 0)) (tail (new-point 0 0)))
     (loop for command in *commands* do
       (move head command)
-      (format t "H: ~a,~a~%" (x head) (y head))
       (let ((tail-move (find-next-tail-move head tail)))
-        (move tail tail-move)
-        (push (format nil "~a,~a" (x tail) (y tail)) positions))
-      (format t "T: ~a,~a~%" (x tail) (y tail)))
+        (unless (point-zerop tail-move)
+          (move tail tail-move)
+          (push (format nil "~a,~a" (x tail) (y tail)) positions))))
     (length (delete-duplicates positions :test #'string=))))
 
